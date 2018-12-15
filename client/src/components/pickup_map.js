c166c778c11c51bf1c4101e7e95574b6c1835cc0
Awet Tsegazeab <avvvet@gmail.com>
2018-12-15 12:56:50 -0800

    ride request in one page;

--- client/src/components/pickup_map.js
+++ client/src/components/pickup_map.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import L from 'leaflet';
-import {Button, Grid, Row, Col, FormControl, FormGroup, ControlLabel} from 'react-bootstrap';
+import {Button, Grid, Row, Col, FormControl, FormGroup, ControlLabel, Badge} from 'react-bootstrap';
 import {NavLink} from 'react-router-dom';
 import * as Nominatim from "nominatim-browser";
 import $ from 'jquery';
@@ -21,6 +21,10 @@ class PickUpMap extends Component {
                 lat: 0,
                 lng: 0
             },
+            route_price : 0,
+            route_distance : 0,
+            route_time : 0,
+            isRouteFound : false,
             map : '',
             markersLayer: '',
             list: [],
@@ -68,7 +72,7 @@ class PickUpMap extends Component {
 
     componentDidMount(){
         var map = L.map('mapid').setView([9.0092, 38.7645], 16);
-       // map.locate({setView: true, maxZoom: 17});
+        map.locate({setView: true, maxZoom: 17});
         
         this.setState({map : map});
 
@@ -85,7 +89,7 @@ class PickUpMap extends Component {
             L.marker(e.latlng).addTo(map)
                 .bindPopup("YOU are" + radius + " meters from this point").openPopup();
         
-            L.circle(e.latlng, radius).addTo(map);
+            //L.circle(e.latlng, radius).addTo(map);
         }
         
         map.on('locationfound', onLocationFound);
@@ -96,10 +100,7 @@ class PickUpMap extends Component {
         
         map.on('locationerror', onLocationError);
 
-        this.getDrivers(map);
-        
-        
-        
+        this.getDrivers(map);  
     };
 
     componentDidUpdate(){
@@ -144,6 +145,88 @@ class PickUpMap extends Component {
                 console.error(error); 
             });        
         }); 
+        
+        if(this.state.dropoff_flag === 'on' && this.state.pickup_flag ==='on' & this.state.isRouteFound === false) {
+            var latlng1 = this.state.pickup_latlng;
+            var latlng2 = this.state.dropoff_latlng;
+            this.findRoute(latlng1, latlng2);
+        }
+    }
+
+    findRoute = (latlng1, latlng2) => {
+        var map = this.state.map;
+        L.Routing.control({
+            waypoints: [
+             L.latLng(latlng1),
+             L.latLng(latlng2)
+            ]
+        })
+        .on('routesfound', this.routeFound)
+        .addTo(map);  
+    }
+    
+    routeFound =(e) => {
+        var routes = e.routes;
+        var _distance = routes[0].summary.totalDistance;
+        var _ride_time = routes[0].summary.totalTime;
+        
+        _distance = Number.parseFloat(_distance/1000).toFixed(2);
+       var  _ride_time_string = timeConvert(Number.parseInt(_ride_time));
+
+        this.setState({
+             route_distance : Number.parseFloat(_distance).toFixed(2),
+             route_price : _ride_price,
+             route_time : _ride_time,
+             isRouteFound : true 
+         });
+
+        function timeConvert(n) {
+            var num = n;
+            var hours = (num / 3600);
+            var rhours = Math.floor(hours);
+            var minutes = (hours - rhours) * 60;
+            var rminutes = Math.round(minutes);
+            
+            var hDisplay = rhours > 0 ? rhours + " hr" : "";
+            var mDisplay = rminutes > 0 ? rminutes + " min" : "";
+            return hDisplay + mDisplay; 
+         }
+        var price_per_km = 19;
+        var _ride_price = Number.parseFloat(_distance * price_per_km).toFixed(2);
+        
+        document.getElementById('ride-price-dashboard').style.visibility = "visible";
+        document.getElementById('ride_price').innerHTML = _ride_price + ' Birr';
+        document.getElementById('distance').innerHTML = _distance + ' km';
+        document.getElementById('ride_time').innerHTML = _ride_time_string;
+    }
+
+    rideRequest = (latlng) => {
+        var objRideRequest = {
+            user_id: '7141',
+            driver_id: '0',
+            pickup_latlng: `POINT(${this.state.pickup_latlng.lat} ${this.state.pickup_latlng.lng})`, 
+            dropoff_latlng: `POINT(${this.state.dropoff_latlng.lat} ${this.state.dropoff_latlng.lng})`,
+            route_distance: this.state.route_distance,
+            route_time: this.state.route_time,
+            route_price: this.state.route_price,
+            status: 1
+        };
+
+        $.ajax({ 
+            type:"POST",
+            url:"/ride/rideRequest",
+            data: JSON.stringify(objRideRequest), 
+            contentType: "application/json",
+            success: function(data, textStatus, jqXHR) {
+                console.log("Nearest driver", data);
+                document.getElementById('div-request').style.visibility = 'visible';
+                this.setState({driver: data});
+                
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(xhr, status, err.toString());
+            }.bind(this)
+        });  
     }
 
     change = (e) => {
@@ -196,6 +279,41 @@ class PickUpMap extends Component {
                    </Row>
                  </Grid>
               </div>
+              
+              <div className="ride-price-dashboard" id="ride-price-dashboard">
+                <div>
+                  <Grid fluid={true}>
+                      <Row>
+                          <Col xs={4} md={4}>
+                            Price <Badge><div id="ride_price"></div></Badge>   
+                          </Col>
+
+                          <Col xs={4} md={4}>
+                           Distance <Badge><div id="distance"></div></Badge>
+                          </Col>
+
+                          <Col xs={4} md={4}>
+                           Time <Badge><div id="ride_time"></div></Badge>
+                          </Col>
+                      </Row>
+
+                      <Row className="rowPadding">
+                          <Col xs={6} sm={6} md={6}>
+                          <Button  onClick={(e) => this.rideRequest(this.state.pickup_latlng, e)} bsStyle="success" bsSize="small">Request ride</Button>
+                          </Col>
+
+                          <Col xs={6} sm={6} md={6}>
+                          <Button  bsStyle="danger" bsSize="small">Cancel ride</Button>
+                          </Col>
+                      </Row>
+
+                  </Grid>
+                  </div>
+                  <div id="div-process" className="div-process">
+                   <div className="div-pickup-btn-box"></div> 
+                  </div>
+              </div>
+
               <div className="mapid" id="mapid"></div>
             </div>
         );
