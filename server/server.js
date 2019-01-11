bfb085833fe9ce6630c00c5484c236f67b9ec2b2
Awet Tsegazeab <avvvet@gmail.com>
2019-01-11 06:21:15 -0800

    fix

--- server/server.js
+++ server/server.js
@@ -44,6 +44,7 @@ const httpsServer = https.createServer(credentials, app);
 var io = socketIO(httpServer);
 
 const clientPath = path.join(__dirname, '../client/build');
+app.use(express.static(clientPath, { dotfiles: 'allow' } ));
 
 app.get('/', (req, res)=>{
   res.sendFile(path.join(clientPath, '/index.html'));
