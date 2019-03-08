7453b0683545b577742c3f76cf2e090674fbbc79
Awet Tsegazeab <avvvet@gmail.com>
2019-03-07 20:59:06 -0800

    update

--- server/server.js
+++ server/server.js
@@ -135,32 +135,6 @@ const upload_user = multer({
     storage: storage_user
 }).single("myImage");
 
-app.post('/user/profile', function (req, res){
-    var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
-    });
-  
-    form.on('end', function(fields, files) {
-      /* Temporary location of our uploaded file */
-      var temp_path = this.openedFiles[0].path;
-      /* The file name of the uploaded file */
-      var file_name = this.openedFiles[0].name;
-      /* Location where we want to copy the uploaded file */
-      var new_location = 'uploads/';
-  
-      fs.copy(temp_path, new_location + file_name, function(err) {  
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("success!")
-        }
-      });
-    });
-});
-  
 app.post('/user/profile', function (req, res) {
     var token = req.header('x-auth');
     var busboy = new Busboy({ headers: req.headers });
