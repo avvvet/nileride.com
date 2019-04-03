cdbf58ec52ad465e54652642d5dc1f831a676799
Awet Tsegazeab <avvvet@gmail.com>
2019-04-03 07:00:36 -0400

    update duration and radius

--- env.js
+++ env.js
@@ -11,7 +11,7 @@ const env = {
   NODE_ENV:'production',
   EMAIL_CC_A: 'avvvet@gmail.com',
   EMAIL_CC_B: 'natimancloud@gmail.com',
-  MISSED_RIDE_DURATION: 15,
-  NEAREST_DRIVER_RADIUS: 1000,
+  MISSED_RIDE_DURATION: 10,
+  NEAREST_DRIVER_RADIUS: 1500,
 };
 module.exports = env;
